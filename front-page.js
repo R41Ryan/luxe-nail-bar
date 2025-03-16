84626e668efc43620f8222786b3288f6e8cb7cf2
R41Ryan <ryanhuynh64@gmail.com>
2025-03-15 23:10:54 -0600

    added basic testimonials

--- front-page.js
+++ front-page.js
@@ -1,12 +1,26 @@
 document
-  .querySelector(".left-scroll-btn")
+  .querySelector("#gallery .left-scroll-btn")
   .addEventListener("click", () => scrollGallery(-1, 400));
 document
-  .querySelector(".right-scroll-btn")
+  .querySelector("#gallery .right-scroll-btn")
   .addEventListener("click", () => scrollGallery(1, 400));
+
+document
+  .querySelector("#testimonials .left-scroll-btn")
+  .addEventListener("click", () => scrollTestimonials(-1, 400));
+document
+  .querySelector("#testimonials .right-scroll-btn")
+  .addEventListener("click", () => scrollTestimonials(1, 400));
+
 
 function scrollGallery(direction, amount) {
   document
-    .querySelector(".gallery-container")
+    .querySelector("#gallery .gallery-container")
+    .scrollBy({ left: direction * amount, behavior: "smooth" });
+}
+
+function scrollTestimonials(direction, amount) {
+  document
+    .querySelector("#testimonials .gallery-container")
     .scrollBy({ left: direction * amount, behavior: "smooth" });
 }
