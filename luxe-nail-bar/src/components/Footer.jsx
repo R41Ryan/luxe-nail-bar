6d42f8ba9150126c4793d4b23ee5bc9fe088dcc1
R41Ryan <ryanhuynh64@gmail.com>
2025-05-01 18:21:56 -0600

    added attribution for background

--- luxe-nail-bar/src/components/Footer.jsx
+++ luxe-nail-bar/src/components/Footer.jsx
@@ -2,8 +2,10 @@ function Footer({ setPage }) {
   return (
     <>
       <footer>
-        <p>116 Government Road North, Diamond Valley <br /> AB, Canada T0L 0H0</p>
-        <p>+1(587) 837-1781</p>
+        <div className="contact-info">
+          <p>116 Government Road North, Diamond Valley <br /> AB, Canada T0L 0H0</p>
+          <p>+1(587) 837-1781</p>
+        </div>
         <ul>
           <a
             onClick={() => {
@@ -34,6 +36,9 @@ function Footer({ setPage }) {
             <li>Health And Safety</li>
           </a>
         </ul>
+        <div className="attribution">
+          <p>Background <a href="http://www.freepik.com">designed by Dotstudio / Freepik</a></p>
+        </div>
       </footer>
     </>
   );
