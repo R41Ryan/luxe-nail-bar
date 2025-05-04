fb13bf585d297321d745c54d9548a3af9b22b945
Ryan Huynh <73033285+R41Ryan@users.noreply.github.com>
2025-05-03 20:35:54 -0600

    created beverage page based on beverage json

--- luxe-nail-bar/src/BeverageMenuPage.jsx
+++ luxe-nail-bar/src/BeverageMenuPage.jsx
@@ -1,91 +1,68 @@
-import Collapsible from "./components/collapsible";
+import { useState, useEffect } from "react";
 
 function BeverageMenuPage() {
+  const [beverageData, setBeverageData] = useState([]);
+  const [beverageHTML, setBeverageHTML] = useState([]);
+  const [selectedCategoryIndex, setSelectedCategoryIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchBeverages = async () => {
+      try {
+        const response = await fetch("JSON/beverages.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setBeverageData(data);
+      } catch (error) {
+        console.error("Error fetching services:", error);
+      }
+    };
+
+    fetchBeverages();
+  }, []);
+
+  useEffect(() => {
+    const generateBeveragesHTML = () => {
+      return beverageData.map((category, categoryIndex) => (
+        <div className="beverage-category-content" key={categoryIndex}>
+          {category.beverages.map((beverage, beverageIndex) => (
+            <div key={beverageIndex} className="beverage-item">
+              <h3 className="beverage-name">{beverage.name}</h3>
+              {beverage.description && (
+                <p className="beverage-description">{beverage.description}</p>
+              )}
+            </div>
+          ))}
+        </div>
+      ));
+    };
+
+    setBeverageHTML(generateBeveragesHTML());
+  }, [beverageData]);
+
   return (
     <main>
       <section id="beverage-menu-section">
         <h2>Our Complimentary Beverages</h2>
+        <p>*1 beverage limit per customer*</p>
         <div className="beverage-menu-container">
-          <Collapsible buttonText={"Mocktails"}>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-          </Collapsible>
-          <Collapsible buttonText={"Soft Drinks"}>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-          </Collapsible>
-          <Collapsible buttonText={"Hot Drinks"}>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-          </Collapsible>
-          <Collapsible buttonText={"Coffee"}>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-          </Collapsible>
-          <Collapsible buttonText={"Smoothies"}>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-            <div className="beverage">
-              <img src="https://placehold.co/100x100" />
-              <h3>Beverage Name</h3>
-              <p>Beverage description</p>
-            </div>
-          </Collapsible>
+          <div className="beverages-categories">
+            {beverageData.map((category, categoryIndex) => (
+              <button
+                key={categoryIndex}
+                className={`category-button ${
+                  selectedCategoryIndex === categoryIndex ? "active" : ""
+                }`}
+                onClick={() => setSelectedCategoryIndex(categoryIndex)}
+              >
+                {category.category}
+              </button>
+            ))}
+          </div>
+          <div className="beverages-content">
+            {beverageHTML[selectedCategoryIndex]}
+          </div>
         </div>
       </section>
     </main>
